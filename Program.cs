08b0b5935516fda8b3ebd6d9b9e8396016e648c6
Meir Yeheskel <meir.yeheskel>
2018-05-15 01:01:47 +0300

    ex1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Show the difference between 2 characters
+
+using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter first character:");
+            int first = (int)(char.Parse(Console.ReadLine()));
+            Console.WriteLine("Please Enter second character:");
+            int second = (int)(Char.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"The difference between the 2 characters is: {Math.Abs(first - second)}");
+
+
+
+        }
+    }
+
+}
